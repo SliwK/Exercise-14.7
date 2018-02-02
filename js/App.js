5e6b2c72d00ba9daa73b98423935fdeb449950aa
Krzysztof Sliwinski <kpsliwinski@gmail.com>
2018-02-02 01:53:33 +0100

    Correct App.js

--- js/App.js
+++ js/App.js
@@ -3,9 +3,10 @@ var App = React.createClass({
 
   render: function() {
     return (
-      React.createElement('div', {className: 'app'},
-        React.createElement(ContactForm, {contact: contactForm}),
-        React.createElement(Contacts, {items: contacts}, {})
+      <div className='app'>
+        <ContactForm contact={contactForm}/>
+        <Contacts items={contacts}/>
+      </div>
       )
     );
   }
